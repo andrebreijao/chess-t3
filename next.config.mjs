cf55220c10a1f3e3139d6f8ba6287a70fa34410e
andrebreijao <andrebreijao@gmail.com>
2022-09-19 20:41:01 -0300

    update cors 3

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,42 @@
-import { env } from "./src/env/server.mjs";
+// import { env } from "./src/env/server.mjs";
 
-/**
- * Don't be scared of the generics here.
- * All they do is to give us autocompletion when using this.
- *
- * @template {import('next').NextConfig} T
- * @param {T} config - A generic parameter that flows through to the return type
- * @constraint {{import('next').NextConfig}}
- */
-function defineNextConfig(config) {
-  return config;
-}
+// /**
+//  * Don't be scared of the generics here.
+//  * All they do is to give us autocompletion when using this.
+//  *
+//  * @template {import('next').NextConfig} T
+//  * @param {T} config - A generic parameter that flows through to the return type
+//  * @constraint {{import('next').NextConfig}}
+//  */
+// function defineNextConfig(config) {
+//   return config;
+// }
 
-export default defineNextConfig({
-  reactStrictMode: true,
-  swcMinify: true,
-  // Next.js i18n docs: https://nextjs.org/docs/advanced-features/i18n-routing
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+// export default defineNextConfig({
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   // Next.js i18n docs: https://nextjs.org/docs/advanced-features/i18n-routing
+//   i18n: {
+//     locales: ["en"],
+//     defaultLocale: "en",
+//   },
+//   async headers() {
+//     return [
+//       {
+//         // matching all API routes
+//         source: "*/api/:path*",
+//         headers: [
+//           { key: "Access-Control-Allow-Credentials", value: "true" },
+//           { key: "Access-Control-Allow-Origin", value: "*" },
+//           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+//           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+//         ]
+//       }
+//     ]
+//   }
+
+// });
+module.exports = {
   async headers() {
     return [
       {
@@ -34,5 +51,4 @@ export default defineNextConfig({
       }
     ]
   }
-
-});
+};
