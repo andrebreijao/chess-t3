36f9f6861fca6ecbc1cab6a348936373f3e6ca1a
andrebreijao <andrebreijao@gmail.com>
2022-09-19 19:28:18 -0300

    migrate backend functions to prisma

--- src/pages/api/boardState.ts
+++ src/pages/api/boardState.ts
@@ -1,34 +1,53 @@
 // src/pages/api/examples.ts
 import type { NextApiRequest, NextApiResponse } from "next";
 import fs from "fs";
+import { prisma } from "../../server/db/client";
 
 const saveBoardState = async (req: NextApiRequest, res: NextApiResponse) => {
-  return new Promise((resolve) => {
-    if (req.method === "GET") {
-      fs.readFile("src/pages/api/boardState.txt", "utf8", (err, data) => {
-        if (err) {
-          console.error(err);
-          res.status(500).send("Error reading file!");
-          return resolve(false);
-        }
-        res.send(data);
-        return resolve(true);
-      });
-    }
+  if (req.method === "GET") {
+    // fs.readFile("src/pages/api/boardState.txt", "utf8", (err, data) => {
+    //   if (err) {
+    //     console.error(err);
+    //     res.status(500).send("Error reading file!");
+    //     return resolve(false);
+    //   }
+    //   res.send(data);
+    //   return resolve(true);
+    // });
 
-    if (req.method === "POST") {
-      const { state } = req.body;
-      if (state) {
-        fs.writeFile("src/pages/api/boardState.txt", state, function (err) {
-          if (err) return console.log(err);
-          console.log("board state updated! > boardState.txt");
-          return resolve(false);
-        });
-        res.send(state);
-        return resolve(true);
-      }
+    // By unique identifier
+    const game = await prisma.games.findUnique({
+      where: {
+        id: "cl87vbxmf00077m4eobs8xfav",
+      },
+    });
+
+    res.send(game?.fen);
+  }
+
+  if (req.method === "POST") {
+    const { state } = req.body;
+    if (state) {
+      // fs.writeFile("src/pages/api/boardState.txt", state, function (err) {
+      //   if (err) return console.log(err);
+      //   console.log("board state updated! > boardState.txt");
+      //   return resolve(false);
+      // });
+      // res.send(state);
+      // return resolve(true);
+
+      const updateGameState = await prisma.games.update({
+        where: {
+          id: "cl87vbxmf00077m4eobs8xfav",
+        },
+        data: {
+          fen: state,
+        },
+      });
+
+      res.send(updateGameState.fen);
     }
-  });
+  }
 };
 
 export default saveBoardState;
--- src/pages/api/newGame.ts
+++ src/pages/api/newGame.ts
@@ -1,23 +1,32 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import fs from "fs";
+// import fs from "fs";
+import { prisma } from "../../server/db/client";
 
 const saveBoardState = async (req: NextApiRequest, res: NextApiResponse) => {
   const initialFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 
-  const writeFileData = (data: string) => {
-    fs.writeFile("src/pages/api/boardState.txt", data, function (err) {
-      if (err) {
-        console.log(err);
-        return false;
-      }
-      console.log("Hello World > colors.txt");
-      return true;
-    });
-  };
+  // const writeFileData = (data: string) => {
+  //   fs.writeFile("src/pages/api/boardState.txt", data, function (err) {
+  //     if (err) {
+  //       console.log(err);
+  //       return false;
+  //     }
+  //     console.log("Hello World > colors.txt");
+  //     return true;
+  //   });
+  // };
 
   if (req.method === "POST") {
-    writeFileData(initialFen);
-    return res.status(200).send(initialFen);
+    // writeFileData(initialFen);
+    const updateGameState = await prisma.games.update({
+      where: {
+        id: "cl87vbxmf00077m4eobs8xfav",
+      },
+      data: {
+        fen: initialFen,
+      },
+    });
+    return res.status(200).send(updateGameState.fen);
   }
 };
 
