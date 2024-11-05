6f2c62ce6ea1f2544cc40b8795044ecf8d4f4225
Andre Breijao <60301471+andrebreijao@users.noreply.github.com>
2024-11-05 13:57:08 +0800

    Change constant notation

--- src/pages/api/newGame.ts
+++ src/pages/api/newGame.ts
@@ -3,7 +3,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../server/db/client";
 
 const saveBoardState = async (req: NextApiRequest, res: NextApiResponse) => {
-  const initialFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+  const INITIAL_FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1" as const;
 
   // const writeFileData = (data: string) => {
   //   fs.writeFile("src/pages/api/boardState.txt", data, function (err) {
@@ -23,7 +23,7 @@ const saveBoardState = async (req: NextApiRequest, res: NextApiResponse) => {
         id: "cl87vbxmf00077m4eobs8xfav",
       },
       data: {
-        fen: initialFen,
+        fen: INITIAL_FEN,
       },
     });
     return res.status(200).send(updateGameState.fen);
